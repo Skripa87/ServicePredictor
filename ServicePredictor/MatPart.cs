932677f24e4f0e581266ba1b42bd39e539cbb133
Alex <skripinalexey1987@gmail.com>
2019-11-14 11:19:29 +0500

    test

--- ServicePredictor/MatPart.cs
+++ ServicePredictor/MatPart.cs
@@ -24,13 +24,14 @@ namespace ServicePredictor
 
         private static point getPoint(double latitude, double longitude)
         {
-            var NH = ((6378245 / Math.Pow(1 - _eccentricitySquad * Math.Pow(Math.Sin(latitude * Math.PI / 180), 2.0),
+            var NH = ((_bigcircleradius / Math.Pow(1 - _eccentricitySquad * Math.Pow(Math.Sin(latitude * Math.PI / 180), 2.0),
                            0.5)) + _altitude);
             var cosA = Math.Cos(latitude * Math.PI / 180);
             var xPoint = NH * cosA * Math.Cos(longitude * Math.PI / 180);
             var yPoint = NH * cosA * Math.Sin(longitude * Math.PI / 180);
             return new point(xPoint, yPoint);
         }
+
         public static double GaversinusMethod(double latitudeA, double latitudeB, double longitudeA, double longitudeB)
         {
             var firstPoint = getPoint(latitudeA, longitudeA);
